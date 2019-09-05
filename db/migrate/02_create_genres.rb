bf547f52b0b1995cd809962e649b57099d05c4fe
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-05 21:37:02 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,8 +1,7 @@
 class CreateGenres < ActiveRecord::Migration[5.2]
-  def change 
+  def change
     create_table :genres do |t|
-      t.string :name
-    end 
-  end 
-      
+      t.string :name 
+    end
+  end
 end
